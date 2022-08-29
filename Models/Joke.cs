fa7db4ec2fd88596610f35ca63d1e7d34ac2936b
Jude52 <jude.ferguson@gmail.com>
2022-08-29 17:05:04 +0100

    adding all files to repo

---
+++ Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        //property (prop) - object of a class. 
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        //Contructor (ctor) - (function) 
+        public Joke()
+        {
+            //leave empty for now. 
+        }
+    }
+}
